b1923045968407a24066c28f15fbcffa0ba50e32
bettynegash <bettynegash0621@gmail.com>
2026-06-24 10:09:51 -0700

    commit

--- src/Member.java
+++ src/Member.java
@@ -1,13 +1,53 @@
 public class Member extends Person{
     private double totalContribution;
     private boolean winner;
+    private double prizeWon;
+    private String equbType;
+    private String nationalId;
+    private String nationality;
     public Member(){
+        this.totalContribution=0;
+        this.winner=false;
+        this.prizeWon=0;
+        this.equbType="Student";
+        this.nationalId=" ";
+        this.nationality="";
 
     }
-    public Member(int id,String name,String phone,double totalContribution,boolean winner){
+
+    public Member(int id,String name,String phone,double totalContribution,boolean winner,
+                  String nationalId,String nationality){
         super(id,name,phone);
         this.totalContribution=totalContribution;
         this.winner=winner;
+        this.prizeWon=0;
+        this.equbType="Student";
+        this.nationalId="";
+        this.nationality="";
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getEqubType() {
+        return equbType;
+    }
+
+    public void setEqubType(String equbType) {
+        this.equbType = equbType;
     }
 
     public double getTotalContribution() {
@@ -25,10 +65,24 @@ public class Member extends Person{
     public void setWinner(boolean winner) {
         this.winner = winner;
     }
+
+    public double getPrizeWon() {
+        return prizeWon;
+    }
+
+    public void setPrizeWon(double prizeWon) {
+        this.prizeWon = prizeWon;
+    }
+
     public String toString(){
         return"ID: "+getId()+"\nName: "+getName()
-                +"\nPhone: "+getPhone()+"\nContribution: "+totalContribution
-                +"\nWinner: "+winner;
+                +"\nPhone: "+getPhone()+
+                "\nNational ID: "+nationalId+
+                "\nNationality: "+nationality+
+                "\nType: "+equbType+
+                "\nContribution: "+totalContribution
+                +"\nWinner Status: : "+(winner ?"Winner ":"Active Contributor")
+                +"\nPrize Won: "+prizeWon+" Birr";
     }
     public void displayRole() {
         System.out.println("Equb Member");
